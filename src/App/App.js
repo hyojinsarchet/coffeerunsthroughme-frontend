545cfba47a4cd61060b8b5d4858ee24e5949a336
hyojin.sarchet <hyojin5931@hanmail.net>
2018-03-27 12:20:07 -0400

    added router in index and app

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Header from "../Header/Header";
 import Table from "../Table/Table";
 import Footer from "../Footer/Footer";
@@ -7,9 +8,13 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Coffee runs through me!</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Table />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
