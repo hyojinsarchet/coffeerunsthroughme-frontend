bc103137e5763f206865cf9305521ae446591e3b
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-03-28 12:41:16 -0400

    fixed nav bar authentication

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,24 +4,40 @@ import "./Header.css";
 
 class Header extends Component {
   render() {
-    return (
-      <div>
+    let nav = [
+      <Link key={2} to="/" className="logo">
+        Coffee Runs Through Me
+      </Link>
+    ];
+    if (this.props.isLoggedIn) {
+      nav.push(
         <div className="header">
-          <Link to="/" className="logo">
+          <Link key={2} to="/" className="logo">
             Coffee Runs Through Me
           </Link>
-          <Link to="/login" className="nav">
-            Login
-          </Link>
-          <Link to="/signup" className="nav">
-            Signup
-          </Link>
-          <Link to="/main" className="nav">
+          <Link key={3} to="/main" className="nav">
             Test
           </Link>
-        </div>
-      </div>
-    );
+          <Link key={4} to="/logout" className="nav">
+            Logout
+          </Link>
+        </div>,
+        console.log(nav)
+      );
+    } else {
+      nav.push(
+        <div className="header">
+          <Link key={5} to="/login" className="nav">
+            Login
+          </Link>
+          <Link key={6} to="/signup" className="nav">
+            Signup
+          </Link>
+        </div>,
+        console.log(nav)
+      );
+    }
+    return <div>{nav}</div>;
   }
 }
 
