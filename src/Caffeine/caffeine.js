4ad8782e6b1d37b435dc67c048f87dfde763be38
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-03-28 16:48:43 -0400

    started caffeine.js component, may need to remove it

---
+++ src/Caffeine/caffeine.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Caffeine extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  handleCaffeine() {
+    if (e.target.value === "coffee") {
+      return;
+    }
+  }
+  render() {
+    return <div> hello </div>;
+  }
+}
