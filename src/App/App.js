3fc392468f9aa4835186374c76081c05d03754bb
Ai Yen Tam <tam.aiyen@gmail.com>
2018-03-30 18:53:05 -0400

    naming conventions

--- src/App/App.js
+++ src/App/App.js
@@ -63,7 +63,7 @@ class App extends Component {
           listOfDrinks.push(newDrink);
         }
         this.setState({
-          drinks: newArray
+          drinks: listOfDrinks
         });
       });
   }
